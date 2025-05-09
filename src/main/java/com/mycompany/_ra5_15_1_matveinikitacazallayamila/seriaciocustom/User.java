f3b079e30186bc50523f9a8b48c78f97c7aae9e5
nikita <nikitamatpech@gmail.com>
2025-05-09 09:39:06 +0200

    Versió inicial del programa

---
+++ src/main/java/com/mycompany/_ra5_15_1_matveinikitacazallayamila/seriaciocustom/User.java
@@ -0,0 +1,11 @@
+package com.mycompany._ra5_15_1_matveinikitacazallayamila.seriaciocustom;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Nikita i Yamila
+ */
+public class User implements Serializable{
+    
+}
---
+++ src/main/java/com/mycompany/_ra5_15_1_matveinikitacazallayamila/seriaciodefault/Hero.java
@@ -0,0 +1,11 @@
+package com.mycompany._ra5_15_1_matveinikitacazallayamila.seriaciodefault;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Nikita i Yamila
+ */
+public class Hero implements Serializable{
+    
+}
