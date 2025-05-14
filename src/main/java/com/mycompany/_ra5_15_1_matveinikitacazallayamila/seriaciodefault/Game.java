3720518ad72b6cb3bc2b3570ffc5fa625938956f
nikita <nikitamatpech@gmail.com>
2025-05-14 20:25:57 +0200

    Possible versió final de Game i Hero

--- src/main/java/com/mycompany/_ra5_15_1_matveinikitacazallayamila/seriaciodefault/Game.java
+++ src/main/java/com/mycompany/_ra5_15_1_matveinikitacazallayamila/seriaciodefault/Game.java
@@ -1,11 +1,48 @@
 package com.mycompany._ra5_15_1_matveinikitacazallayamila.seriaciodefault;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 /**
  *
  * @author Nikita i Yamila
  */
-public class Game implements Serializable{
+public class Game implements Serializable {
     
+    // Declaració d'atribust
+    private int score;
+    private Hero player;
+    private int level;
+    
+    // Declaració de constants
+    private final static int SCORE_DEFAULT = 0;
+    private final static int LEVEL_DEFAULT = 1;
+    private final static Hero PLAYER_DEFAULT = new Hero();
+    
+    /**
+     * 
+     */
+    public Game() {
+        this(PLAYER_DEFAULT);
+    }
+    
+    public Game(Hero jugador){
+        this.score = SCORE_DEFAULT;
+        this.player = jugador;
+        this.level = LEVEL_DEFAULT;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Hero getPlayer() {
+        return player;
+    }
+
+    public int getLevel() {
+        return level;
+    }
 }
--- src/main/java/com/mycompany/_ra5_15_1_matveinikitacazallayamila/seriaciodefault/Hero.java
+++ src/main/java/com/mycompany/_ra5_15_1_matveinikitacazallayamila/seriaciodefault/Hero.java
@@ -6,6 +6,72 @@ import java.io.Serializable;
  *
  * @author Nikita i Yamila
  */
-public class Hero implements Serializable{
+public class Hero implements Serializable {
     
+    // Declaració d'atributs
+    private String name;
+    private transient char gender;
+    private String type;
+    
+    // Declaració de constants
+    private final static String NAME_DEFAULT = "Player";
+    private final static char GENDER_DEFAULT = 'm';
+    private final static String TYPE_DEFAULT = "Warrior";
+    
+    /**
+     * Constructor per defecte que truca al parametritzat
+     */
+    public Hero (){
+        this(NAME_DEFAULT, GENDER_DEFAULT, TYPE_DEFAULT);
+    }
+    
+    /**
+     * Constructor parametritzat que li dona un nom, sexe i el tipus de personatge
+     * que és l'heroi.
+     * 
+     * @param name nom del heroi
+     * @param gendere sexe del heroi
+     * @param type el tipus de personatge
+     */
+    public Hero(String name, char gendere, String type){
+        this.name = name;
+        this.gender = gendere;
+        this.type = type;
+    }
+    
+    /**
+     * Mètode per saber el nom de l'heroi
+     * 
+     * @return nom de l'heroi
+     */
+    public String getName() {
+        return name;
+    }
+    
+    /**
+     * Mètode per saber el sexe de l'heroi
+     * 
+     * @return sexe del heroi
+     */
+    public char getGendere() {
+        return gender;
+    }
+    
+    /**
+     * Mètode per saber el tipus de personatge que és l'heroi
+     * 
+     * @return tipus de personatge
+     */
+    public String getType() {
+        return type;
+    }
+    
+    /**
+     * Mètode per canviar el nom de l'heroi
+     * 
+     * @param name nom nou
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 }
